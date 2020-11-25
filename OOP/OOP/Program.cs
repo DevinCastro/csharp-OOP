7756aeb10423dc185aa5dc9d52b5d4c3f84c7433
DevinCastro <dcrocker21@gmail.com>
2020-11-25 06:20:15 -0800

    classes, constructors

--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -2,11 +2,27 @@
 
 namespace OOP
 {
+
+    public class Vehicle
+    {
+        public int numPassengers;
+
+        // CLASS CONSTRUCTOR
+        public Vehicle(int num)
+        {
+            numPassengers = num;
+        }
+    }
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Vehicle mustang = new Vehicle(5);
+
+            Console.WriteLine($"Hello World! My car can hold {mustang.numPassengers} people");
         }
     }
 }
