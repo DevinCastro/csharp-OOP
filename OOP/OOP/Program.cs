3ba336a866c2358bc6c4400cbaba7b7a11d69e76
DevinCastro <dcrocker21@gmail.com>
2020-11-25 07:28:18 -0800

    Human class.  fields, methods/constructors, properties

--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -14,15 +14,67 @@ namespace OOP
         }
     }
 
+    public class Human
+    {
+        //fields
+        public string name;
+        public int stregnth;
+        public int intelligence;
+        public int dexterity;
+        private int health;
+
+        public int Health
+        {
+            get
+            {
+                return health;
+            }
+        }
+
+        public Human(string humanName)
+        {
+            name = humanName;
+            stregnth = 3;
+            intelligence = 3;
+            dexterity = 3;
+            health = 100;
+        }
+
+        public Human(string Name, int Str, int Int, int Dex, int Healthh )
+        {
+            name = Name;
+            stregnth = Str;
+            intelligence = Int;
+            dexterity = Dex;
+            health = Healthh;
+        }
+
+        public int Attack(Human target)
+        {
+            target.health = target.health - (5 * stregnth);
+            return target.Health;
+        }
+    }
+
+
+
 
 
     class Program
     {
         static void Main(string[] args)
         {
-            Vehicle mustang = new Vehicle(5);
 
-            Console.WriteLine($"Hello World! My car can hold {mustang.numPassengers} people");
+            Human Devin = new Human("Devin");
+            Human Christy = new Human("Christy", 5, 5, 2, 150);
+
+            Console.WriteLine(Christy.Attack(Devin));
+            Console.ReadLine();
+
+
+            //Vehicle mustang = new Vehicle(5);
+
+            //Console.WriteLine($"Hello World! My car can hold {mustang.numPassengers} people");
         }
     }
 }
