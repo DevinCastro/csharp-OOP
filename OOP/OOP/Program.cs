d739dbe726b0619781a62f92f17f95d2fbe87f97
DevinCastro <dcrocker21@gmail.com>
2020-11-30 09:41:01 -0800

    More on classes, constructors, getters,

--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OOP
 {
@@ -56,6 +57,98 @@ namespace OOP
         }
     }
 
+    public class Food
+    {
+        public string Name;
+        public int Calories;
+        public bool IsSpicy;
+        public bool IsSweet;
+
+        public Food(string name, int cal, bool isSpicy, bool isSweet)
+        {
+            Name = name;
+            Calories = cal;
+            IsSpicy = isSpicy;
+            IsSweet = isSweet;
+        }
+    }
+
+    public class Buffet
+    {
+        public List<Food> Menu;
+
+        public Buffet()
+        {
+            Menu = new List<Food>()
+            {
+                new Food("Sushi", 300, true, false),
+                new Food("steak", 300, false, false),
+                new Food("chicken", 300, false, false),
+                new Food("Shrimp", 300, true, false),
+                new Food("noodles", 500, true, false),
+                new Food("banana", 100, false, true),
+                new Food("apple", 100, false, true),
+            };
+
+        }
+        public Food Serve()
+        {
+            Random rand = new Random();
+            Food food = Menu[rand.Next(1, 7)];
+            return food;
+        }
+
+    }
+
+    public class Ninja
+    {
+        private int calorieIntake;
+        public List<Food> FoodHistory = new List<Food>();
+
+        public void Cal()
+        {
+            calorieIntake = 0;
+        }
+
+        public bool IsFull
+        {
+            get
+            {
+                if (calorieIntake > 1200)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+                
+            }
+        }
+
+        public void Eat(Food food)
+        {
+            if (IsFull)
+            {
+                Console.WriteLine("This Ninja is Full.");
+            }
+            else
+            {
+                calorieIntake = calorieIntake + food.Calories;
+                FoodHistory.Add(food);
+                Console.WriteLine($"{food.Name}; Spicy:{food.IsSpicy}; Sweet:{food.IsSweet}.");
+                Console.WriteLine(calorieIntake);
+
+                for (int i = 0; i < FoodHistory.Count; i++)
+                {
+                    Console.WriteLine(FoodHistory[i].Name);
+                }
+                Console.WriteLine(FoodHistory.Count);
+            }
+
+        }
+
+    }
 
 
 
@@ -64,11 +157,24 @@ namespace OOP
     {
         static void Main(string[] args)
         {
+            Buffet buffet = new Buffet();
+            //Console.WriteLine(buffet.Serve());
+            Ninja devin = new Ninja();
+            devin.Eat(buffet.Serve());
+            devin.Eat(buffet.Serve());
+            devin.Eat(buffet.Serve());
+            devin.Eat(buffet.Serve());
+            devin.Eat(buffet.Serve());
+            devin.Eat(buffet.Serve());
 
-            Human Devin = new Human("Devin");
-            Human Christy = new Human("Christy", 5, 5, 2, 150);
 
-            Console.WriteLine(Christy.Attack(Devin));
+
+
+
+            //Human Devin = new Human("Devin");
+            //Human Christy = new Human("Christy", 5, 5, 2, 150);
+            //Console.WriteLine(Christy.Attack(Devin));
+            //Console.WriteLine(Devin.Attack(Christy));
             Console.ReadLine();
 
 
