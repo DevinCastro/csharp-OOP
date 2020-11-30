ef8bcef2f7d6bab173288d5db8b124c55f8f8e1c
DevinCastro <dcrocker21@gmail.com>
2020-11-30 10:52:23 -0800

    added deck of cards

--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -151,21 +151,180 @@ namespace OOP
     }
 
 
+    public class Card
+    {
+        private string stringVal;
+        private string suit;
+        private int val;
+
+        public Card(string StringVal, string Suit, int Val)
+        {
+            stringVal = StringVal;
+            suit = Suit;
+            val = Val;
+        }
+    }
+
+    public class Deck
+    {
+        public List<Card> cards = new List<Card>()
+        {
+            new Card("Ace", "Spades", 1),
+            new Card("2", "Spades", 2),
+            new Card("3", "Spades", 3),
+            new Card("4", "Spades", 4),
+            new Card("5", "Spades", 5),
+            new Card("6", "Spades", 6),
+            new Card("7", "Spades", 7),
+            new Card("8", "Spades", 8),
+            new Card("9", "Spades", 9),
+            new Card("10", "Spades", 10),
+            new Card("Jack", "Spades", 11),
+            new Card("Queen", "Spades", 12),
+            new Card("King", "Spades", 13),
+            new Card("Ace", "Clubs", 1),
+            new Card("2", "Clubs", 2),
+            new Card("3", "Clubs", 3),
+            new Card("4", "Clubs", 4),
+            new Card("5", "Clubs", 5),
+            new Card("6", "Clubs", 6),
+            new Card("7", "Clubs", 7),
+            new Card("8", "Clubs", 8),
+            new Card("9", "Clubs", 9),
+            new Card("10", "Clubs", 10),
+            new Card("Jack", "Clubs", 11),
+            new Card("Queen", "Clubs", 12),
+            new Card("King", "Clubs", 13),
+            new Card("Ace", "Hearts", 1),
+            new Card("2", "Hearts", 2),
+            new Card("3", "Hearts", 3),
+            new Card("4", "Hearts", 4),
+            new Card("5", "Hearts", 5),
+            new Card("6", "Hearts", 6),
+            new Card("7", "Hearts", 7),
+            new Card("8", "Hearts", 8),
+            new Card("9", "Hearts", 9),
+            new Card("10", "Hearts", 10),
+            new Card("Jack", "Hearts", 11),
+            new Card("Queen", "Hearts", 12),
+            new Card("King", "Hearts", 13),
+            new Card("Ace", "Diamonds", 1),
+            new Card("2", "Diamods", 2),
+            new Card("3", "Diamods", 3),
+            new Card("4", "Diamods", 4),
+            new Card("5", "Diamods", 5),
+            new Card("6", "Diamods", 6),
+            new Card("7", "Diamods", 7),
+            new Card("8", "Diamods", 8),
+            new Card("9", "Diamods", 9),
+            new Card("10", "Diamonds", 10),
+            new Card("Jack", "Diamonds", 11),
+            new Card("Queen", "Diamonds", 12),
+            new Card("King", "Diamonds", 13),
+        };
+        
+        public Card deal()
+        {
+            cards.Remove(cards[0]);
+
+            return cards[0];
+        }
+
+        public void reset()
+        {
+            cards = new List<Card>()
+        {
+            new Card("Ace", "Spades", 1),
+            new Card("2", "Spades", 2),
+            new Card("3", "Spades", 3),
+            new Card("4", "Spades", 4),
+            new Card("5", "Spades", 5),
+            new Card("6", "Spades", 6),
+            new Card("7", "Spades", 7),
+            new Card("8", "Spades", 8),
+            new Card("9", "Spades", 9),
+            new Card("10", "Spades", 10),
+            new Card("Jack", "Spades", 11),
+            new Card("Queen", "Spades", 12),
+            new Card("King", "Spades", 13),
+            new Card("Ace", "Clubs", 1),
+            new Card("2", "Clubs", 2),
+            new Card("3", "Clubs", 3),
+            new Card("4", "Clubs", 4),
+            new Card("5", "Clubs", 5),
+            new Card("6", "Clubs", 6),
+            new Card("7", "Clubs", 7),
+            new Card("8", "Clubs", 8),
+            new Card("9", "Clubs", 9),
+            new Card("10", "Clubs", 10),
+            new Card("Jack", "Clubs", 11),
+            new Card("Queen", "Clubs", 12),
+            new Card("King", "Clubs", 13),
+            new Card("Ace", "Hearts", 1),
+            new Card("2", "Hearts", 2),
+            new Card("3", "Hearts", 3),
+            new Card("4", "Hearts", 4),
+            new Card("5", "Hearts", 5),
+            new Card("6", "Hearts", 6),
+            new Card("7", "Hearts", 7),
+            new Card("8", "Hearts", 8),
+            new Card("9", "Hearts", 9),
+            new Card("10", "Hearts", 10),
+            new Card("Jack", "Hearts", 11),
+            new Card("Queen", "Hearts", 12),
+            new Card("King", "Hearts", 13),
+            new Card("Ace", "Diamonds", 1),
+            new Card("2", "Diamods", 2),
+            new Card("3", "Diamods", 3),
+            new Card("4", "Diamods", 4),
+            new Card("5", "Diamods", 5),
+            new Card("6", "Diamods", 6),
+            new Card("7", "Diamods", 7),
+            new Card("8", "Diamods", 8),
+            new Card("9", "Diamods", 9),
+            new Card("10", "Diamonds", 10),
+            new Card("Jack", "Diamonds", 11),
+            new Card("Queen", "Diamonds", 12),
+            new Card("King", "Diamonds", 13),
+        };
+        }
+}
+
 
 
     class Program
     {
         static void Main(string[] args)
         {
-            Buffet buffet = new Buffet();
-            //Console.WriteLine(buffet.Serve());
-            Ninja devin = new Ninja();
-            devin.Eat(buffet.Serve());
-            devin.Eat(buffet.Serve());
-            devin.Eat(buffet.Serve());
-            devin.Eat(buffet.Serve());
-            devin.Eat(buffet.Serve());
-            devin.Eat(buffet.Serve());
+
+            Deck myDeck = new Deck();
+            Console.WriteLine(myDeck.deal());
+            Console.WriteLine(myDeck.deal());
+            Console.WriteLine(myDeck.deal());
+            Console.WriteLine(myDeck.deal());
+
+            Console.WriteLine(myDeck.cards.Count);
+
+            myDeck.reset();
+            Console.WriteLine(myDeck.cards.Count);
+
+
+
+
+
+
+
+
+
+            // hungry ninja, use Food, Buffet, and Ninja classes
+            //Buffet buffet = new Buffet();
+            //Ninja devin = new Ninja();
+            //devin.Eat(buffet.Serve());
+            //devin.Eat(buffet.Serve());
+            //devin.Eat(buffet.Serve());
+            //devin.Eat(buffet.Serve());
+            //devin.Eat(buffet.Serve());
+            //devin.Eat(buffet.Serve());
 
 
 
